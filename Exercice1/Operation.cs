77256d76f98186acb81695f3df3e4e154ed62f1a
Ntemo-Vanguard <140407707+Ntemo-Vanguard@users.noreply.github.com>
2023-11-29 04:15:26 +0000

    Add files via upload

---
+++ Exercice1/Operation.cs
@@ -0,0 +1,51 @@
+﻿namespace Exercice1
+{
+    public class Operation
+    {
+
+        /// <summary>
+        ///  cette methode permet de faire l'addition
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int addition(int a,int b)
+        {
+            //retourne la somme de a et b
+            return a + b;
+        }
+        /// <summary>
+        /// Cette methode permet de faire la soustraction
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int soustraction(int a, int b)
+        {
+            //retourne la difference de a et b
+            return a - b;
+        }
+        /// <summary>
+        /// Cette methode permet de faire la multiplication
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int multiplication(int a, int b)
+        {
+            //retourne la multiplication de a et b
+            return a * b;
+        }
+        /// <summary>
+        /// Cette methode permet de faire la division
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int division(int a, int b)
+        {
+            //retourne la division de a et b
+            return a / b;
+        }
+    }
+}
---
+++ Exercice1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Xml;
+using Exercice1;
+
+
+            int a, b;
+            string getRep, op;
+            bool rep = false;
+
+            Operation ope = new Operation();
+
+            Console.Write("Tu peux faire des operations avec ce programme :\n");
+            
+            do
+            {
+                do
+                {
+                    Console.Write("Entrer le 1er nombre :  \r\n");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Entrer le 2eme nombre :  \r\n");
+                    b = Convert.ToInt32(Console.ReadLine());
+
+                }while(a <= 0 && b <= 0);
+                Console.WriteLine("Votre operation, mon frere : ");
+                Console.WriteLine("1- 'A' pour l'addition \n 2- 'B' pour la soustraction \n 3- 'C' pour la multiplication \n 4- 'D' pour la division");
+                op = Console.ReadLine();
+
+
+            switch (op){
+                        case "a":
+                            Console.WriteLine("{0} + {1} = {2}", a, b, ope.addition(a,b));
+                            break;
+                        case "b":
+                            Console.WriteLine("{0} - {1} = {2}", a, b, ope.soustraction(a, b));
+                            break;
+                        case "c":
+                            Console.WriteLine("{0} x {1} = {2}", a, b, ope.multiplication(a, b));
+                            break;
+                        case "d":
+                            Console.WriteLine("{0} / {1} = {2}", a, b, ope.division(a, b));
+                            break;
+             }
+    
+    
+                 Console.WriteLine("Saisissez z pour faire un autre calcul ou x pour quitter");
+                 getRep = Console.ReadLine();
+                 rep = getRep.ToUpper()=="Z"?true:false;
+            } while (rep);
